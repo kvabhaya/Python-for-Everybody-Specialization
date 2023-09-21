d06c95064b9a15245be09a4bf9f639ab22fb6e55
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-09-21 14:40:08 +0530

    Create Filtering-in-a-Loop.py

---
+++ Programming for Everybody (Getting Started with Python)/Week 07/Loop Idioms/Filtering-in-a-Loop.py
@@ -0,0 +1,14 @@
+#Filtering in a Loop
+x=[1,2,3,4,5]
+for i in x:
+    if i<5:
+        print(i,"Less than 5")
+    else:
+        print(i,"Exactly equal to 5")
+
+
+# 1 Less than 5
+# 2 Less than 5
+# 3 Less than 5
+# 4 Less than 5
+# 5 Exactly equal to 5
